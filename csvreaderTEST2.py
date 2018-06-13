e1d2ecec0984e13c2d44b56a7a45fdcae2022747
Nathaniel D'Souza <nathanields@gmail.com>
2018-06-13 11:32:40 -0700

    Update csvreaderTEST2.py
    fixes doubling of first line, still needs to fix the reading of unneeded lines in the csv, definitely need to  QA this ( build addition to throw large amounts of csv data at it to see what works?)

--- csvreaderTEST2.py
+++ csvreaderTEST2.py
@@ -10,22 +10,22 @@ def num_check(s): #num_check trys the value 's' to see if it's a float (number) 
         except ValueError:
             return False
 
-
-def header_check(header,headerFull):
+def header_check(header,headerFull,i): #header_check builds the headers into a single list to be parsed into the sql server,
     s = 0
-    print(len(header))
-    print(len(headerFull))
     while s < len(header):
-        headerFull[s] = headerFull[s] + header[s]
-        s = s+1
+        if i > 0:
+            headerFull[s] = headerFull[s] + '/' + header[s]
+            s = s+1
+        else:
+            s = s+1
     return headerFull
-
 
 def csv_reader(): #csv_reader opens a csv, calls it into memory, and checks the first 10 rows to see if data is there. If it is data, all previous rows stored to
                   #memory are concatenated and become the header (complete_header). 
     import csv
     #f1="/media/sf_ndsouza/testDATA/Julysept20145MIN1.csv"
-    f1="/media/sf_ndsouza/testDATA/July_Sept_2014_Health.csv"
+    #f1="/media/sf_ndsouza/testDATA/July_Sept_2014_Health.csv"
+    f1="/media/sf_ndsouza/testDATA/July_Sept_2014_FiveMin.csv"
     with open(f1, 'rt') as dataFILE: #opens file and sets up reader and iterator
         csvreader = csv.reader(dataFILE)
         headerList = (next(csvreader))
@@ -35,22 +35,24 @@ def csv_reader(): #csv_reader opens a csv, calls it into memory, and checks the 
         while i < 10:  #amount of rows to check, change this number to check more or less rows
             if num_check(headerVal) == False: #calls num_check (see above function). If this row is not data, function saves it (incase it is the header), then
                                               #iterates to the next row and preps the first value for checking
-                i = i+1
                 header = headerList
-                complete_header = header_check(header,headerFull)
+                complete_header = header_check(header,headerFull,i)
                 headerList = (next(csvreader))
                 headerVal = headerList[0]
+                i = i+1
                 ##print('help! Im stuck in a while loop and cant get out!')
             elif num_check(headerVal) == True: #if num_check returns true, this is where the database header building function will be called and implemented. 
                                                #Once it is complete, the loop breaks and the function ends. This should actually ideally concatenate multiple
                                                #rows since it seems a lot of these files use more than one row as a header, with units and other info included
                 #print(header)
                 #print(i)
-                print("oh god finally, Im free from these mortal coils")
-                print(headerFull)
+                #print("oh god finally, Im free from these mortal coils")
+                print(complete_header)
+                return (complete_header)
                 #headerFull = header_check(i,f1) #-header check is the function which will find all rows of headers and concatenate them into a single list (headerFull)
                 break                
 
 
 csv_reader()
 
+#def sanitize_values(headerFull):
