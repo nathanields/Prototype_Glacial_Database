80037a98d245d14f5277806eb974060694133d34
Nathaniel D'Souza <nathanields@gmail.com>
2018-09-12 14:06:15 -0700

    sql injection testing

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
-headerList = ['aaaa','bbbb','cccc','dddd']
-headerString = ', '.join('%s' * len(headerList))
-print(headerString)
-headerString = "'" + headerString + "'"
-print(headerString)
-queryString = 'INSERT INTO table VALUES (%s);' %headerString 
+import psycopg2
 
-final = queryString,headerList
+conn = psycopg2.connect("host=localhost dbname=testDB user=ndsouza password=glacier1")
+cur = conn.cursor()
+
+headerList = ['aa/aa','bb(bb','cc)cc','d.ddd']
+headerString = "%s," * len(headerList)
+
+headerString = headerString[:-1]
+
+#print(headerString)
+#headerString = "'" + headerString + "'"
+
+final = 'INSERT INTO values VALUES (%s);' %headerString 
+
 print(final)
+print(final, headerList)
+
+cur.execute(final, headerList)
+conn.commit
+print('complete')
